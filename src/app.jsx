e77717dcff1cb323c966de9c36eab295ee2d5bd2
Lee-SeongHyun316 <mn0316@naver.com>
2021-11-22 11:58:36 +0900

    [ADD] firebase, auth

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import './app.css';
+import Login from "./components/login/login";
 
-function App() {
-    return <h1>Hello :)</h1>;
+function App({authService}) {
+    return <Login authService={authService}/>;
 }
 export default App;
