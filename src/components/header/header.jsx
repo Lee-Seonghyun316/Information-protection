21bcf6197b4bfd4e07d8e8391edbcfe7920ab7ec
Lee-SeongHyun316 <mn0316@naver.com>
2021-11-22 15:10:42 +0900

    [ADD] Header UI/UX

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import styles from './header.module.css';
 
-const Header = (props) => (
-    <h1>header</h1>
+const Header = ({onLogout}) => (
+    <header className={styles.header}>{onLogout && <button className={styles.logout} onClick={onLogout}>Logout</button>}
+        <div className={styles.headerLine}></div>
+        <h1 className={styles.title}>Information-Protection</h1>
+    </header>
 );
 
 export default Header;
