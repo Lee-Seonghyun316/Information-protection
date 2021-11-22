a3a95a9984980905bb958b0273fb91ed7539669e
Lee-SeongHyun316 <mn0316@naver.com>
2021-11-22 15:30:08 +0900

    [ADD] logout

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import styles from './header.module.css';
 
 const Header = ({onLogout}) => (
-    <header className={styles.header}>{onLogout && <button className={styles.logout} onClick={onLogout}>Logout</button>}
+    <header className={styles.header}>
+        {true && <button className={styles.logout} onClick={onLogout}>Logout</button>}
         <div className={styles.headerLine}></div>
         <h1 className={styles.title}>Information-Protection</h1>
     </header>
