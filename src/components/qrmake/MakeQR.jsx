13ab928e734a0279e896e53087bbee6339316005
Lee-SeongHyun316 <mn0316@naver.com>
2021-12-08 15:57:28 +0900

    [ADD] 암호화 복호화 알고리즘 추가

--- src/components/qrmake/MakeQR.jsx
+++ src/components/qrmake/MakeQR.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { useNavigate } from 'react-router';
+import React, {useEffect, useRef, useState} from 'react'
+import {useNavigate} from 'react-router';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './qrmake.module.css';
+import CryptoJS from "crypto-js";
 
 const MakeQR = ({authService, infoRepository}) => {
     const navigate = useNavigate();
@@ -13,16 +14,43 @@ const MakeQR = ({authService, infoRepository}) => {
     const [address, setAddress] = useState('');
     const imgRef = useRef();
 
+    const encrypt = (data, key) => {
+        console.log(data, "text1");
+        const result = CryptoJS.AES.encrypt(JSON.stringify(data), key).toString();
+        console.log(result, "text2");
+        return result;
+    }
+    const decrypt = (text, key) => {
+        try {
+            console.log(text, "decryption1")
+            const bytes = CryptoJS.AES.decrypt(text, key);
+            const result = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+            console.log(result, "decryption2");
+            return result;
+        } catch (err) {
+            console.error(err);
+            return;
+        }
+    }
+
+    const data = {
+        name: "test",
+        age: 30,
+        phone: "44444444",
+    };
+
+    const text = encrypt(data, "secret-key-1");
+    decrypt(text, "secret-key-1");
+
     const onLogout = () => {
         authService.logout();
     }
 
     useEffect(() => {
         authService.onAuthChange(user => {
-            if(user){
+            if (user) {
                 setUserId(user.uid);
-            }
-            else {
+            } else {
                 navigate('/');
             }
         });
@@ -33,14 +61,14 @@ const MakeQR = ({authService, infoRepository}) => {
         const url = `https://chart.googleapis.com/chart?cht=qr&chl=${name}${phone}${address}&chs=300x300`;
         imgRef.current.src = url;
         const QR = {
-            id : Date.now(),
-            url : url,
+            id: Date.now(),
+            url: url,
         }
         infoRepository.saveInfo(userId, QR);
     }
     return (
         <section className={styles.section}>
-            <Header onLogout={onLogout} />
+            <Header onLogout={onLogout}/>
             <div className={styles.container}>
                 <div className={styles.wrapper}>
                     <div className={styles.formBox}>
@@ -63,12 +91,12 @@ const MakeQR = ({authService, infoRepository}) => {
                     </div>
                     <div className={styles.info}>
                         <div className={styles.box}>
-                            <img ref={imgRef} src="" alt="" className={styles.img} />
+                            <img ref={imgRef} src="" alt="" className={styles.img}/>
                         </div>
                     </div>
                 </div>
             </div>
-            <Footer />
+            <Footer/>
         </section>
     )
 }
